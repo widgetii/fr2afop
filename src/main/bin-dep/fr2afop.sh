de5184ccad7ff1586ac7e80b019b0dff977a6367
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-07-23 23:03:50 +0300

    fixed bash script absolute path with OSX

--- src/main/bin-dep/fr2afop.sh
+++ src/main/bin-dep/fr2afop.sh
@@ -1,13 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+# BASHMODE="readlink"
 
 #--------------------------------------------
 #  set parameters
 #--------------------------------------------
 
+if [ "$BASHMODE" = "readlink" ] ; then
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f $0)
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTDIR=$(dirname $SCRIPT)
+else
+# Absolute path to this script
+SCRIPT="${BASH_SOURCE[0]}"
+# Absolute path this script is in
+SCRIPTDIR="$(cd "$(dirname "${SCRIPT}")" ; pwd)"
+fi
+# echo "Script path: ${SCRIPTDIR}/$(basename "${SCRIPT}")"
 # Change dir to script dir
 cd $SCRIPTDIR
 
